8618bc5bd8faccd78134c240a7396cb19959ca7f
Georgi Yankov <georgi.yankov@takeaway.com>
2020-04-11 16:30:42 +0300

    Configure docker containers to run app locally

--- config/logging.php
+++ config/logging.php
@@ -43,7 +43,7 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/' . php_sapi_name() . '/laravel.log'),
             'level' => 'debug',
         ],
 
