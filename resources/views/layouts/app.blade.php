82d8f354da8656b782c2ed1fe4a42e48880ed51b
Georgi Yankov <georgi.yankov@takeaway.com>
2020-04-14 12:23:48 +0300

    [FE] Working on templates and layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,98 +1,99 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-    <body class="bg-gray-400 font-sans tracking-normal" style="line-height: 0;">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="bg-gray-400 font-sans tracking-normal">
 
-    <div id="app" v-cloak>
-        <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <a class="text-white no-underline hover:text-white hover:no-underline" href="{{url('/')}}">
-                <span class="text-2xl pl-2"><i class="em em-grinning"></i>Svara Planing</span>
-            </a>
-        </div>
+        <div id="app" v-cloak>
+            <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
+                <div class="flex items-center flex-shrink-0 text-white mr-6">
+                    <a class="text-white no-underline hover:text-white hover:no-underline" href="{{url('/')}}">
+                        <span class="text-2xl pl-2"><i class="em em-grinning"></i>Svara Planing</span>
+                    </a>
+                </div>
 
-        <div class="block lg:hidden">
-            <button id="nav-toggle"
-                    class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>
-                        Menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                </svg>
-            </button>
-        </div>
+                <div class="block lg:hidden">
+                    <button id="nav-toggle"
+                            class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-300 hover:text-white hover:border-white">
+                        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>
+                                Menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                        </svg>
+                    </button>
+                </div>
 
-        <div class="w-full flex-grow lg:flex items-center w-auto block" id="nav-content">
-            <ul class="list-reset lg:flex justify-end flex-1 items-center">
-                <li class="mr-3 pt-2">
-                    <a class="inline-block px-4 text-white no-underline" href="{{route('voting_room')}}">Voting Room</a>
-                </li>
-                <li class="mr-3 pt-2">
-                    <a class="inline-block px-4 text-white no-underline" href="{{route('history')}}">History</a>
-                </li>
-                <li class="mr-3 pt-2">
-                    <a class="inline-block px-4 text-white no-underline" href="{{route('scrum_planing')}}">Scrum
-                        Planing</a>
-                </li>
-                @guest
-                    <li class="mr-3 pt-2">
-                        <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
-                           href="{{route('login')}}">{{__('Login')}}</a>
-                    </li>
-                    @if(Route::has('register'))
+                <div class="w-full flex-grow lg:flex items-center w-auto block" id="nav-content">
+                    <ul class="list-reset lg:flex justify-end flex-1 items-center">
                         <li class="mr-3 pt-2">
-                            <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
-                               href="{{route('register')}}">{{ __('Register') }}</a>
+                            <a class="inline-block px-4 text-white no-underline" href="{{route('voting_room')}}">Voting Room</a>
                         </li>
-                    @endif
-                @else
-                    <li class="mr-3 pt-2">
-                        <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
-                           href="{{url('/home')}}">{{__('Home')}}</a>
-                    </li>
-                    <li class="mr-3 pt-2">
-                        <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
-                           v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-                        <div>
-                            <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                               document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+                        <li class="mr-3 pt-2">
+                            <a class="inline-block px-4 text-white no-underline" href="{{route('history')}}">History</a>
+                        </li>
+                        <li class="mr-3 pt-2">
+                            <a class="inline-block px-4 text-white no-underline" href="{{route('scrum_planing')}}">Scrum
+                                Planing</a>
+                        </li>
+                        @guest
+                            <li class="mr-3 pt-2">
+                                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
+                                   href="{{route('login')}}">{{__('Login')}}</a>
+                            </li>
+                            @if(Route::has('register'))
+                                <li class="mr-3 pt-2">
+                                    <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
+                                       href="{{route('register')}}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="mr-3 pt-2">
+                                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
+                                   href="{{url('/home')}}">{{__('Home')}}</a>
+                            </li>
+                            <li class="mr-3 pt-2">
+                                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
+                                   v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div>
+                                    <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline px-4"
+                                       href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
 
-            </ul>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="py-4 shadow-lg mx-auto mt-20">
+                @yield('content')
+            </main>
         </div>
-    </nav>
-
-        <main class="py-4 container shadow-lg mx-auto mt-20">
-            @yield('content')
-        </main>
-</div>
-</body>
+    </body>
 </html>
--- resources/views/pages/voting_room.blade.php
+++ resources/views/pages/voting_room.blade.php
@@ -1,6 +1,95 @@
 @extends('layouts.app');
 @section('content')
-    <div>
-        dzag voting room, give your voice now!
+    <div class="flex justify-between mt-4">
+        <div class="flex flex-col justify-center w-full h-full">
+            <div class="flex">
+                <div id="cards-options" class="flex flex-col items-center content-center justify-center w-1/12 border border-2 border-red-600">
+                    <div class="py-2 px-4 w-1/2 items-center">0</div>
+                    <div class="py-2 px-4 w-1/2 items-center">1/2</div>
+                    <div class="py-2 px-4 w-1/2 items-center">3</div>
+                    <div class="py-2 px-4 w-1/2 items-center">5</div>
+                    <div class="py-2 px-4 w-1/2 items-center">8</div>
+                    <div class="py-2 px-4 w-1/2 items-center">13</div>
+                    <div class="py-2 px-4 w-1/2 items-center">20</div>
+                    <div class="py-2 px-4 w-1/2 items-center">40</div>
+                </div>
+                <div  id="bet-table" class="flex flex-col justify-between w-full">
+                <div class="flex flex-col flex-wrap items-center relative">
+
+                    <div class="flex flex-col w-1/2">
+                        <p>Story description goes here : </p>
+                        <span class="absolute right-0 mr-10">Time: 00:00 </span>
+                    </div>
+                    <div class="flex justify-evenly w-1/2 mt-20 flex-wrap">
+                        <div class="">X</div>
+                        <div class="">X</div>
+                        <div class="">X</div>
+                        <div class="">X</div>
+                        <div class="">X</div>
+                        <div class="">X</div>
+                        <div class="">X</div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div id="player-seats" class="flex items-center justify-evenly mt-10">
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+                <div class="flex flex-col items-center border border-2 border-red-400 w-1/12">
+                    <span>icon</span>
+                    <span>vote</span>
+                    <span>Name</span>
+                </div>
+            </div>
+        </div>
+
+        <section id="dealer-seat" class="border items-center border-2 border-red-500">
+            <div class="flex flex-col h-auto m-2">
+                <div class="w-full justify-evenly">
+                    <span class="w-1/3">Round</span>
+                    <button class="w-1/3">Start</button>
+                    <button class="w-1/3">End</button>
+                </div>
+                <span class="w-full flex justify-center">Planing start </span>
+                <span class="w-full flex justify-center">Planing End </span>
+
+                <span class="my-4">Results</span>
+                <div>
+                    <div>
+                        <span>[00:00]</span>
+                        <span>the story description</span>
+                        <span>[estimation]</span>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
